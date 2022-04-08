c4de85047f4d9805819408240d62e907fa7c529e
fw <fw@fw>
2022-04-08 18:19:32 +0600

    Implement solveEquation function

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
 function solveEquation(a, b, c) {
   let arr;
+
   // код для задачи №1 писать здесь
+  let d = b * b - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
   return arr; // array
 }
 
