14826bbfaa4641483a01e92b101b630fb96a57ec
fw <fw@fw>
2022-04-08 18:50:09 +0600

    Implement compareArrays and advancedFilter functions

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
   // Ваш код
+  if (arr1.length !== arr2.length)
+    return false;
 
-  return result; // boolean
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+
+  return true;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr
+    .filter(val => val >= 0 && val % 3 === 0)
+    .map(val => val * 10);
 
   return resultArr; // array
 }
